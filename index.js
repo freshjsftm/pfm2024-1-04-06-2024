06bef04bcab95e1c55a3c2690b08b828da9fa214
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-06 09:46:59 +0300

    Add readFile promises

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const fs = require('fs');
+const fs = require('fs/promises');
 //console.log(fs);
 
-fs.readFile('./text.txt', 'utf8',(err, data)=>{
-	if(err){
-		console.log(err);
-		return;
-	}
-	console.log(data);
+fs.readFile('./text.txt', 'utf-8')
+.then((data)=>{
+	console.log(data)
+})
+.catch((err)=>{
+	console.log(err)
 })

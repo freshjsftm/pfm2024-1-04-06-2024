efa689c8c13a49eac4157f1d0e1beb3e895805f7
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-06 09:39:07 +0300

    Add readFile callback

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 'use strict';
 
-const { Article } = require('./components/Article');
-const { Button } = require('./components/Button');
+const fs = require('fs');
+//console.log(fs);
 
-const article = new Article();
-article.render();
-
-const btn = new Button();
-btn.render();
-
-//require
-//resolving
-// 1 - core modules
-// 2 - file  *.js | *.json
-// 3 - dir   package.json-> "main" | index.js | index.json
-// 4 - node_modules
-// 5 - throw new Error()
+fs.readFile('./text.txt', 'utf8',(err, data)=>{
+	if(err){
+		console.log(err);
+		return;
+	}
+	console.log(data);
+})

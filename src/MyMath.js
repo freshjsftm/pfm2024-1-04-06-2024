ec1de8d5b15e22a11b4ac57acef2fb804b8e4752
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-06-07 10:40:50 +0300

    Add import at MyMath

--- src/MyMath.js
+++ src/MyMath.js
@@ -1,9 +1,12 @@
+import { f1 } from "./utils";
+
 class MyMath {
   static sum = (a, b) => a + b;
   static sub = (a, b) => a - b;
   static del = (a, b) => a / b;
   static mult = (a, b) => a * b;
 }
+f1();
 console.log('inside My Math file!!!');
 
 //export commonJS 
